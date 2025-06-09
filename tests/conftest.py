e3cef8c84252d9a77facce8f83f89da8d90c3b6d
DianaSfl <eee.oo.ee@mail.ru>
2025-06-09 14:36:49 +0300

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import os
+import tempfile
+
 import pytest
 from selenium import webdriver
 
@@ -11,9 +14,12 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="session")
 def login_page(request):
     url = request.config.getoption('--url')
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--window-size=1920x1080")
-    driver = webdriver.Chrome()
+    user_data_dir = os.path.join(tempfile.mkdtemp(), "chrome_profile")
+    options = webdriver.ChromeOptions()
+    options.add_argument(f"--user-data-dir={user_data_dir}")
+    options.add_argument("--no-sandbox")  # Важно для GitHub Actions!
+    options.add_argument("--disable-dev-shm-usage")  # Для избежания ошибок памяти
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
     login_page = loginPage(driver)
     yield login_page
